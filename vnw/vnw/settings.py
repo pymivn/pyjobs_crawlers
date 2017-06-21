2830f4dd6214e6a198991293f5aaad07e0a0c9f8
huyhoang17 <hoangphan0710@gmail.com>
2017-06-21 23:25:52 +0700

    add scrapy-splash setting

--- vnw/vnw/settings.py
+++ vnw/vnw/settings.py
@@ -7,16 +7,33 @@ BOT_VERSION = '1.0.5'
 SPIDER_MODULES = ['vnw.spiders']
 NEWSPIDER_MODULE = 'vnw.spiders'
 ITEM_PIPELINES = {
-        'vnw.pipelines.ValidatePipeline': 500,
+    'vnw.pipelines.ValidatePipeline': 500,
 }
 
 USER_AGENT = ("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML,"
               " like Gecko) Chrome/48.0.2564.116 Safari/537.36")
 
+SPLASH_URL = 'http://localhost:8050'
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
+SPLASH_COOKIES_DEBUG = True
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    ('scrapy.downloadermiddlewares.httpcompression.'
+     'HttpCompressionMiddleware'): 810,
+}
+
 is_prod = os.environ.get('PYJOBS_IS_PROD', False)
 if is_prod:
     ITEM_PIPELINES.update({
-            'vnw.pipelines.APIPipeline': 1000,
+        'vnw.pipelines.APIPipeline': 1000,
     })
     DOWNLOAD_DELAY = 2
     DOWNLOADER_MIDDLEWARES = {
